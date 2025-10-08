9ea0a5959def4ed4d62658430ce4eb8302a84b71
4canzz <4canzz@gmail.com>
2025-10-08 23:40:06 +0500

    added problem statement for LAB-5

--- LAB-5/Problem-1/main.cpp
+++ LAB-5/Problem-1/main.cpp
@@ -17,3 +17,52 @@ int main()
     }
     return 0;
 }
+
+/*Exercise 1: Implement Stack using singly linked list. 
+Implement all the methods given in the following class definition for Stack using a singly linked
+list  as the underlying data structure. Your implementation should work for the main function  
+that follows. 
+#ifndef STACK_H  
+#define STACK_H 
+template<class KeyType>  
+class Stack 
+{ //  
+public: 
+    // constructor , creates an empty stack  
+    Stack(); 
+    // returns true if Stack is full, otherwise return false  
+    bool IsFull(); 
+    //If number of elements in the Stack is zero return true,  
+    otherwise return false 
+    bool IsEmpty(); 
+    // If Stack is not full, insert item into the Stack. Must be an  O(1) operation 
+    void Push(const KeyType item); 
+    // If Stack is empty return 0 or NULL; 
+    // else return appropriate item from the Stack. Must be an O(1)  operation 
+    KeyType Pop(); 
+private: 
+    SList <KeyType>* list;  
+    Node<KeyType>* top; 
+}; 
+#endif
+
+#include"Stack.h"  
+#include"Stack.cpp"  
+#include<iostream>  
+using namespace std; 
+int main() 
+{ 
+    Stack<int> *st =new Stack<int>(); 
+    if(st->IsEmpty()) 
+        cout<<"Stack is currently empty"<<endl; 
+    st->Push(1); 
+    st->Push(2); 
+    st->Push(3); 
+    while (!st->IsEmpty()) 
+    { 
+        int value=st->Pop();  
+        cout<<value<<endl; 
+    } 
+    return 0; 
+}
+*/
--- LAB-5/Problem-2/main.cpp
+++ LAB-5/Problem-2/main.cpp
@@ -15,3 +15,14 @@ int main() {
     delete stack;
     return 0;
 }
+
+/*Exercise 2: Application of Stack 
+A postfix expression is an expression in which each operator follows its operands. The advantage of
+this form is that there is no need to group sub-expressions in parentheses or to consider operator
+precedence. Stack can be used to evaluate a postfix (or prefix) expression. Please implement the
+C++ code given below to evaluate a postfix expression. 
+1. Make a left to right scan of the postfix expression 
+2. If the element is an operand push it on Stack 
+3. If the element is operator, evaluate it using as operands the correct number from stack and
+pushing the result onto the stack 
+*/
